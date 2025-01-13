3fa1a6f1283b460aa57f04fc9f32d5b35a766e85
akshay/770 <sonarakshay873@gmail.com>
2025-01-13 23:58:51 +0530

    new changes

--- PizzaHut_Sales_Analysis/sql_file.sql
+++ PizzaHut_Sales_Analysis/sql_file.sql
@@ -1,3 +1,25 @@
+-- Identify the most pizza size ordered ?
+
+SELECT 
+    pizzas.size,
+    COUNT(order_details.order_details_id) AS order_count
+FROM
+    pizzas
+        JOIN
+    order_details ON pizzas.pizza_id = order_details.pizza_id
+GROUP BY pizzas.size
+ORDER BY order_count DESC;
+
+
+
+-- Identify the highest - priced pizza ?
+
+
+SELECT pizza_types.name, pizzas.price
+FROM pizza_types JOIN pizzas
+on pizza_types.pizza_type_id = pizzas.pizza_type_id
+order by pizzas.price desc limit 1;
+
 -- Determine the top 3 most ordered pizza types based on revenue for each pizza category.
 
 select name, revenue from 
